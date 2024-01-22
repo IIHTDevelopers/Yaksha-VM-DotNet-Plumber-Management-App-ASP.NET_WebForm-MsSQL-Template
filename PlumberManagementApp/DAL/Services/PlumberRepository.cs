4a6688781d6f9726dea45185ca2021d3e22e96a4
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-22 15:59:47 +0530

    Add files via upload

---
+++ PlumberManagementApp/DAL/Services/PlumberRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using PlumberManagementApp.DAL.Interfaces;
+
+namespace PlumberManagementApp.DAL.Services
+{
+    public class PlumberRepository : Interfaces.IPlumberRepository
+    {
+        private PlumberDbContext _context;
+
+        public PlumberRepository(PlumberDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.PlumberModel GetById(string id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PlumberManagementApp/DAL/Services/PlumberService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using PlumberManagementApp.DAL.Interfaces;
+using PlumberManagementApp.Model;
+
+namespace PlumberManagementApp.DAL.Services
+{
+    public class PlumberService : Interfaces.IPlumberService
+    {
+        private Interfaces.IPlumberRepository _repository;
+
+        public PlumberService(Interfaces.IPlumberRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public PlumberModel GetById(string id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
